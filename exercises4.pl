1165efbf80b20a45a8f40d13ab7afa3d002bdf6f
Natasa Brisudova <98270916+natasabrisudova@users.noreply.github.com>
2022-04-11 13:35:37 +0200

    Create exercises4.pl

---
+++ exercises4.pl
@@ -0,0 +1,56 @@
+% Write a program to check if the number of elements
+% in two binary trees T1 and T2 is the same.
+
+count(nil,0).                         % the end
+count(t(_,nil,nil),1).                % at node
+count(t(X,T1,T2),S):- count(T1,S1), 
+                      count(T2,S2), 
+                      S is 1+S1+S2.   % add 1 for X
+                      
+equal_el_in_tree([],[]).
+equal_el_in_tree(t(X,T1,T2),t(Y,B1,B2)):- count(t(X,T1,T2),S1), 
+                                          count(t(Y,B1,B2),S2),
+                                          S1==S2.
+
+
+
+% Write a program to select from an input list of integers 
+% all odd values.
+
+odd([],[]).
+odd([X|L],LO):- 0 is mod(X,2),      % if X is even, ignore    
+                odd(L, LO).
+odd([X|L],[X|LO]):- 1 is mod(X,2),  % if X is odd, copy to result   
+                  odd(L, LO).
+
+
+
+% Write a program to check if two NOT ordered input vectors
+% contain same elements.
+
+list_same_length([], []).
+list_same_length([_|L1], [_|L2]) :- list_same_length(L1, L2).
+
+member(X, [X|_]).
+member(X, [_|T]) :- member(X, T).
+
+omit(X, [X|T], T).   
+omit(X, [H|T], [H|R]) :- omit(X, T, R).
+
+same_elements([], []).
+same_elements([H|T], L2) :-   list_same_length([H|T],L2), 
+                              member(H,L2),
+                              omit(H,L2,LO),       %LO=L2-{H}
+                              same_elements(T,LO).
+
+
+
+% Write a program which checks if two lists (L1 and L2) of integer
+% values are consecutive (for one element in L1 there exists one
+% which is its successor in L2).
+
+% Checking two consecutive elements of a list
+next_to(X,Y,[X,Y|_]).
+next_to(X,Y,[_|Z]) :- next_to(X,Y,Z).
+
+
