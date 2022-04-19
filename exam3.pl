0e230bcfac73fd4e7f43a8855e2eff4bf197266b
Natasa Brisudova <98270916+natasabrisudova@users.noreply.github.com>
2022-04-19 18:26:37 +0200

    Update exam3.pl

--- exam3.pl
+++ exam3.pl
@@ -54,13 +54,13 @@ append_without_rep([X|L1],L2,[X|LO]) :- append_without_rep(L1,L2,LO).
 intersectionTrees(nil,_,[]).
 intersectionTrees(_,nil,[]).
 intersectionTrees(t(X,T1,T2),T3,[X|L]) :-  intersectionTrees(T1,T3,L1),
-    									                     intersectionTrees(T2,T3,L2),
-    									                     append_without_rep(L1,L2,L),
-    									                     searchNinTree(X,T3).
+                                           intersectionTrees(T2,T3,L2),
+                                           append_without_rep(L1,L2,L),
+                                           searchNinTree(X,T3).
 intersectionTrees(t(X,T1,T2),T3,L) :- intersectionTrees(T1,T3,L1),
-    									                intersectionTrees(T2,T3,L2),
-    									                append_without_rep(L1,L2,L),
-    									                not(searchNinTree(X,T3)).
+                                      intersectionTrees(T2,T3,L2),
+                                      append_without_rep(L1,L2,L),
+                                      not(searchNinTree(X,T3)).
                               		  
     		
                          
