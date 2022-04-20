0a857c0c34dabdcae41552997370c3b130e48478
Natasa Brisudova <98270916+natasabrisudova@users.noreply.github.com>
2022-04-20 08:58:13 +0200

    Update exam1.pl

--- exam1.pl
+++ exam1.pl
@@ -4,7 +4,6 @@
 % of values, and two integer numbers N, M, 
 % returns an ordered list LO containing all elements in L plus N and M,
 % without repetitions.
-
 % So, for instance ascendingorder([-5,-3,0,1,1,4,7],7,2,[-5,-3,0,1,2,4,7]) is true.
 
 % remove duplicates
@@ -25,16 +24,3 @@ ascendingorder(L,N1,N2,LO):- insert(N1,L,L1),
                              remove_duplicates(L2,LO).
                              
    
-% ----------------------------------------------------------------------------------
-% EXAM SOLUTION (? - not really working when there are already duplicates in list):
-
-insert_noDuplication(X,[],[X]).
-insert_noDuplication(X,[X|L],[X|L]) :- !.
-insert_noDuplication(X, [Y|T], [X,Y|T]) :- X < Y, !.
-insert_noDuplication(X, [Y|T0], [Y|T])  :- X>Y,
-                             insert_noDuplication(X, T0, T).
-
-ascendingorder(L,N1,N2,L2):- insert_noDuplication(N1,L,L1),
-                             insert_noDuplication(N2,L1,L2).
-
-                               
