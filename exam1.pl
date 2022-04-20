c7961f4e6c0f7904d7b462a16ddf15e3ffb23e66
Natasa Brisudova <98270916+natasabrisudova@users.noreply.github.com>
2022-04-20 08:58:48 +0200

    Update exam1.pl

--- exam1.pl
+++ exam1.pl
@@ -6,21 +6,18 @@
 % without repetitions.
 % So, for instance ascendingorder([-5,-3,0,1,1,4,7],7,2,[-5,-3,0,1,2,4,7]) is true.
 
-% remove duplicates
+
 remove_duplicates([], []).
 remove_duplicates([H|T],R) :- member(H,T), !,
                               remove_duplicates(T,R).
-remove_duplicates([H|T],[H|R]) :- % if H is not a member of T
-                                  remove_duplicates(T,R).
+remove_duplicates([H|T],[H|R]) :- remove_duplicates(T,R).
 
-% insert X in its correct position in a sorted list
 insert(X, [], [X]).
 insert(X, [Y|T], [X,Y|T]) :- X < Y, !.
-insert(X, [Y|T0], [Y|T])  :- % if Y=<X then insert X in tail
-                             insert(X, T0, T).
+insert(X, [Y|T0], [Y|T])  :- insert(X, T0, T).
     
 ascendingorder(L,N1,N2,LO):- insert(N1,L,L1),
                              insert(N2,L1,L2),
                              remove_duplicates(L2,LO).
                              
-   
+  
